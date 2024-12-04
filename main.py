7ecf7aea24df457d4ddd84f04b1a173563c36b93
Dominique Dessy <ddessy@splunk.com>
2024-12-04 09:54:04 -0500

    minimize repetitions

--- main.py
+++ main.py
@@ -3,38 +3,35 @@ from dnac_client import DNACClient, DNAC_HOST
 from splunk_hec import SplunkHEC
 
 
-def main():
-    # Print the list of all network devices
-    # One a t the time in a compact way
+def get_and_send_data(endpoint, splunk_hec, source, sourcetype):
+    client = DNACClient(endpoint)
+    data = client.get_data()
+    splunk_hec.send_events_batch(data, source=source, sourcetype=sourcetype)
 
+
+def main():
     # Create a HEC Connection
     splunk_hec = SplunkHEC(splunk_host="localhost", splunk_port="8088")
 
     # Call the individual APIs and send the results to Splunk
-    device_health_client = DNACClient("/dna/intent/api/v1/device-health")
-    device_health_data = device_health_client.get_data()
-
-    for item in device_health_data:
-        event = json.dumps(item, indent=None, separators=(",", ":"))
-        splunk_hec.send_event(
-            event, source=DNAC_HOST, sourcetype="cisco:dnac:device_health"
-        )
-
-    client_health_client = DNACClient("/dna/intent/api/v1/client-health")
-    client_health_data = client_health_client.get_data()
-    for item in client_health_data:
-        event = json.dumps(item, indent=None, separators=(",", ":"))
-        splunk_hec.send_event(
-            event, source=DNAC_HOST, sourcetype="cisco:dnac:client_health"
-        )
-
-    network_device_client = DNACClient("/dna/intent/api/v1/network-device")
-    network_device_data = network_device_client.get_data()
-    for item in network_device_data:
-        event = json.dumps(item, indent=None, separators=(",", ":"))
-        splunk_hec.send_event(
-            event, source=DNAC_HOST, sourcetype="cisco:dnac:network_devices"
-        )
+    get_and_send_data(
+        "/dna/intent/api/v1/device-health",
+        splunk_hec,
+        DNAC_HOST,
+        "cisco:dnac:device_health",
+    )
+    get_and_send_data(
+        "/dna/intent/api/v1/client-health",
+        splunk_hec,
+        DNAC_HOST,
+        "cisco:dnac:client_health",
+    )
+    get_and_send_data(
+        "/dna/intent/api/v1/network-device",
+        splunk_hec,
+        DNAC_HOST,
+        "cisco:dnac:network_devices",
+    )
 
 
 if __name__ == "__main__":
